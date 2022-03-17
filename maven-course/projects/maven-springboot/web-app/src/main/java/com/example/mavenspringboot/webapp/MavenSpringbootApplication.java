b1cc82be0f68fabc6ca56be01305a590a72c1105
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-03-17 13:10:38 +0530

    Multi-Module Springbooot project

--- maven-course/projects/maven-springboot/web-app/src/main/java/com/example/mavenspringboot/webapp/MavenSpringbootApplication.java
+++ maven-course/projects/maven-springboot/web-app/src/main/java/com/example/mavenspringboot/webapp/MavenSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.example.mavenspringboot;
+package com.example.mavenspringboot.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
