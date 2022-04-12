fb07659743a5ed97378f710c927ec45628a93bc1
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-04-12 18:10:25 +0530

    Configured API Gateway with CircuitBreaker

--- spring-microservices/projects/order-my-food/gateway-service/src/main/java/com/tastyfood/gateway/service/config/RouteConfig.java
+++ spring-microservices/projects/order-my-food/gateway-service/src/main/java/com/tastyfood/gateway/service/config/RouteConfig.java
@@ -1,30 +1,31 @@
 package com.tastyfood.gateway.service.config;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class RouteConfig {
 
-    @Autowired
-    private RouteDefinitionLocator locator;
-
     @Bean
-    public List<GroupedOpenApi> apis() {
-        List<GroupedOpenApi> groups = new ArrayList<>();
-        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-        assert definitions != null;
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
-            String name = routeDefinition.getId().replaceAll("-service", "");
-            groups.add(GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build());
-        });
-        return groups;
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/order-management/**")
+                        .filters(f -> f.circuitBreaker(cb -> cb.setName("OrderMgmtFallback")
+                                .setFallbackUri("forward:/fallback")
+                                .addStatusCode("NOT_FOUND")
+                                .addStatusCode("INTERNAL_SERVER_ERROR")
+                                .setRouteId("OrderMgmtFallback")))
+                        .uri("lb://order-management-service"))
+                .route(p -> p.path("/restaurant-search/**")
+                        .filters(f -> f.circuitBreaker(cb -> cb.setName("RestaurantSearchFallback")
+                                .setFallbackUri("forward:/fallback")
+                                .addStatusCode("NOT_FOUND")
+                                .addStatusCode("INTERNAL_SERVER_ERROR")
+                                .setRouteId("RestaurantSearchFallback")))
+                        .uri("lb://restaurant-search-service"))
+                .build();
     }
+
 }
--- spring-microservices/projects/order-my-food/restaurant-search-service/src/main/java/com/tastyfood/restaurant/service/controller/HomeController.java
+++ spring-microservices/projects/order-my-food/restaurant-search-service/src/main/java/com/tastyfood/restaurant/service/controller/HomeController.java
@@ -1,15 +1,19 @@
 package com.tastyfood.restaurant.service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/restaurant-search")
+@Slf4j
 public class HomeController {
 
     @GetMapping
     public String home() {
+        log.info("From HomeController");
+
         return "Restaurant Search Service";
     }
 
