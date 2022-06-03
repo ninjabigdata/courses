6c9e20de301c908b17797366b4daced6be98a420
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-06-03 19:05:33 +0530

    Enable Logging for Netty as Textual Representational

--- spring-5/Projects/sfg-restful-brewery-master/src/main/java/guru/springframework/sfgrestbrewery/config/WebClientConfig.java
+++ spring-5/Projects/sfg-restful-brewery-master/src/main/java/guru/springframework/sfgrestbrewery/config/WebClientConfig.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgrestbrewery.config;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class WebClientConfig {
@@ -13,7 +15,9 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl(WebClientProperties.BASE_URL)
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.create().wiretap(true)))
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()
+                        .wiretap("reactor.netty.client.httpClient", LogLevel.TRACE,
+                                AdvancedByteBufFormat.TEXTUAL)))
                 .build();
     }
 
