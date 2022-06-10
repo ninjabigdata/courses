362e841f540e273b9f2827dda05492a4c56e23ae
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2022-06-10 16:21:24 +0530

    Initial Commit - Reactive Streams Project

---
+++ spring-5/Projects/streaming-stock-quote-service/src/test/java/com/spring/reactive/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.reactive.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
